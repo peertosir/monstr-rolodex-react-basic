fec5831c388a710d2ecb8942ecd6530366e3b655
Ilya Danilov <peertosir@gmail.com>
2020-05-30 19:32:51 +0300

    Updates

---
+++ precache-manifest.ae0eb088cbe9a92af7d3af946f43069f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029c1ff7c29901c9424f305274ef6ddb",
+    "url": "/monstr-rolodex-react-basic/index.html"
+  },
+  {
+    "revision": "6fa3c54f00d9f7995cbb",
+    "url": "/monstr-rolodex-react-basic/static/css/main.40440cbb.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monstr-rolodex-react-basic/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstr-rolodex-react-basic/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa3c54f00d9f7995cbb",
+    "url": "/monstr-rolodex-react-basic/static/js/main.6b0e2c5b.chunk.js"
+  },
+  {
+    "revision": "2191bb67bc52aa7823e9",
+    "url": "/monstr-rolodex-react-basic/static/js/runtime-main.b7d2de9d.js"
+  }
+]);
